5e654dcf3f7e17b1a020cb0e471536b8bd67d85d
SerdarHELLI <62758782+SerdarHelli@users.noreply.github.com>
2023-04-11 11:39:08 +0300

    changedataset url

--- download_dataset.py
+++ download_dataset.py
@@ -10,7 +10,7 @@ from zipfile import ZipFile
 from io import BytesIO
 
 def download_dataset(save_path):
-    r = requests.get("https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/hxt48yk462-1.zip")
+    r = requests.get("https://prod-dcd-datasets-cache-zipfiles.s3.eu-west-1.amazonaws.com/hxt48yk462-1.zip")
     print("Downloading...")
     z = ZipFile(BytesIO(r.content))    
     z.extractall(save_path)
